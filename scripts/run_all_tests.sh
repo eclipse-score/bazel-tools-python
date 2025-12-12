7dab254bdd969ade2a66b452d8984467f29beb87
jan.nowotsch <jan.nowotsch@bmw.de>
2025-12-12 11:11:56 +0100

    [tests] add workspace modes
    Extend run_all_tests to support an optional argument that allows to
    select which tests to run, either all, only tests in the main workspace
    or only tests in the test workspace.
    
    This shall help to reduce the CI resource usage. In particular the
    amount of disk storage needed, if only either of the test workspaces is
    executed on a separate CI node.

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -22,6 +22,7 @@ RESET='\033[0m'
 declare -A command_status
 declare -a command_order
 exit_code=0
+[ $# -gt 0 ] && workspace=$1 || workspace="all"
 
 
 interrupt_handler(){
@@ -49,51 +50,57 @@ run_command(){
 	command_order+=("$name")
 }
 
-# Run bazel test with workspace mode and python 3.9.
-# TODO re-enable once the score targets (like //:docs) support bazel workspace mode and more than python 3.12
-# run_command "bazel --output_base=$HOME/.cache/bazel_tools_python/workspace_output_base test --config=use_workspace_mode --config=python_3_9 //..." "tests (workspace mode and python 3.9)"
+if [ "${workspace}" == "all" -o "${workspace}" == "main" ];then
+	# Run bazel test with workspace mode and python 3.9.
+	# TODO re-enable once the score targets (like //:docs) support bazel workspace mode and more than python 3.12
+	# run_command "bazel --output_base=$HOME/.cache/bazel_tools_python/workspace_output_base test --config=use_workspace_mode --config=python_3_9 //..." "tests (workspace mode and python 3.9)"
 
-# Run bazel test with bzlmod mode and python 3.12.
-run_command "bazel --output_base=$HOME/.cache/bazel_tools_python/python_3_12_output_base test --config=python_3_12 //..." "tests (bzlmod mode and python 3.12)"
+	# Run bazel test with bzlmod mode and python 3.12.
+	run_command "bazel --output_base=$HOME/.cache/bazel_tools_python/python_3_12_output_base test --config=python_3_12 //..." "tests (bzlmod mode and python 3.12)"
 
-# Run bazel test with bzlmod mode.
-run_command "bazel test //..." "tests (bzlmod mode)"
+	# Run bazel test with bzlmod mode.
+	run_command "bazel test //..." "tests (bzlmod mode)"
 
-# Run python quality tools.
-run_command "bazel build --config=ruff_check --keep_going //..." "ruff_check"
-run_command "bazel build --config=ruff_format --keep_going //..." "ruff_format"
-run_command "bazel build --config=pylint --keep_going //..." "pylint"
-run_command "bazel build --config=black --keep_going //..." "black"
-run_command "bazel build --config=isort --keep_going //..." "isort"
-run_command "bazel build --config=mypy --keep_going //..." "mypy"
+	# Run python quality tools.
+	run_command "bazel build --config=ruff_check --keep_going //..." "ruff_check"
+	run_command "bazel build --config=ruff_format --keep_going //..." "ruff_format"
+	run_command "bazel build --config=pylint --keep_going //..." "pylint"
+	run_command "bazel build --config=black --keep_going //..." "black"
+	run_command "bazel build --config=isort --keep_going //..." "isort"
+	run_command "bazel build --config=mypy --keep_going //..." "mypy"
 
-# Run test workspace tests.
-run_command "test/run_all_tests.sh" "tests (in test workspace)"
+	# Run buildifier.
+	run_command "bazel run bazel/buildifier:check" "buildifier"
 
-# Run buildifier.
-run_command "bazel run bazel/buildifier:check" "buildifier"
+	# Run Eclipse-specific checks.
+	run_command "bazel run //:copyright.check -- --fix" "eclipse copyright check"
+	run_command "bazel test //:format.check" "eclipse format check"
 
-# Run Eclipse-specific checks.
-run_command "bazel run //:copyright.check -- --fix" "eclipse copyright check"
-run_command "bazel test //:format.check" "eclipse format check"
+	# Run security vulnerability scan.
+	run_command "third_party/pip/check_vulnerabilities.sh" "security scan"
+fi
 
-# Run security vulnerability scan.
-run_command "third_party/pip/check_vulnerabilities.sh" "security scan"
+if [ "${workspace}" == "all" -o "${workspace}" == "test" ];then
+	# Run test workspace tests.
+	run_command "test/run_all_tests.sh" "tests (in test workspace)"
+fi
 
-# Print execution summary
-printf '%-37s | %-10s\n' "Command Name" "Status"
-printf '%-37s | %-10s\n' "-------------------------------------" "----------"
+if [ "${workspace}" == "all" -o "${workspace}" == "main" ];then
+	# Print execution summary
+	printf '%-37s | %-10s\n' "Command Name" "Status"
+	printf '%-37s | %-10s\n' "-------------------------------------" "----------"
 
-for name in "${command_order[@]}"; do
-	status="${command_status[$name]}"
+	for name in "${command_order[@]}"; do
+		status="${command_status[$name]}"
 
-	if [[ "$status" == "SUCCEEDED" ]]; then
-		printf "%-37s | ${GREEN}%-10s${RESET}\n" "$name" "$status"
-	else
-		printf "%-37s | ${RED}%-10s${RESET}\n" "$name" "$status"
-	fi
-done
+		if [[ "$status" == "SUCCEEDED" ]]; then
+			printf "%-37s | ${GREEN}%-10s${RESET}\n" "$name" "$status"
+		else
+			printf "%-37s | ${RED}%-10s${RESET}\n" "$name" "$status"
+		fi
+	done
 
-printf '%-37s | %-10s\n' "-------------------------------------" "----------"
+	printf '%-37s | %-10s\n' "-------------------------------------" "----------"
+fi
 
 exit $exit_code
